bb13fb655b0751ac29e306af49b5e6e9b25541e9
abhishek <asevarik@gmail.com>
2024-05-06 00:14:36 +0530

    added delete functionality

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -2,50 +2,83 @@ import { useForm } from "react-hook-form";
 import { CardInput, FormHeader, SideBar } from "./components";
 import { Flashcard } from "./data";
 import LabelledInput from "./components/LabelledInput";
+import { useState } from "react";
 
 export type FormValues = {
-    title: string;
-    card: Flashcard[];
+  title: string;
+  card: Flashcard[];
 };
 
 const HomePage = () => {
-    const form = useForm<FormValues>();
+  const form = useForm<FormValues>();
 
-    const { register, control, handleSubmit, formState } = form;
-    const { errors } = formState;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = form;
+  const handleSaveCard = (formValues: any) => {
+    console.log("Submitted Values :- ", formValues);
+  };
+  const [cardDeck, setCardDeck] = useState<Flashcard[]>([
+    { question:"1", answer: "1", queue: "unseen" },
+  ]);
+  const handleAddMoreCards = ()=>{
+    setCardDeck(prev=>[...prev,{ question: `${prev.length+1}`, answer: `${prev.length+1}`, queue: "unseen" }])
+  } 
+  const handleDeleteCard = (card:Flashcard)=>{
+    
+    setCardDeck(prev=>prev.filter((flashcard:Flashcard)=>flashcard.question !==card.question))
+  }
+  return (
+    <div className="container grid grid-cols-2 gap-12 mx-auto p-4 ">
+      <SideBar />
+      <div className="grid gap-4">
+        <FormHeader onCancel={() => {}} onSave={handleSubmit(handleSaveCard)}>
+          <LabelledInput
+            id="title"
+            label="title"
+            placeholder="Enter the Deck title here..."
+            register={register}
+            registrationParams="title"
+            registrationOptions={{required:"this field is required"}}
+            errors={errors}
+          />
+        </FormHeader>
+        {cardDeck.map((card: Flashcard, index: number) => (
+          <CardInput key={`${card.question}-${index}`} onDelete={() => handleDeleteCard(card)} orderNumber={index+1}>
+            <LabelledInput
+              label="Question"
+              id={`question-${index}`}
+              placeholder="Place Question here"
+              register={register}
+              registrationParams={`card.${index}.question`}
+              registrationOptions={{required:"this field is required"}}
+              errors={errors}
+              inputIdx={index}
+            />
+            <LabelledInput
+              label="Answer"
+              id={`answer-${index}`}
+              placeholder="Place the Correct Answer Here..."
+              register={register}
+              errors={errors}
+              registrationParams={`card.${index}.answer`}
+              registrationOptions={{required:"this field is required"}}
+              inputIdx={index}
+            />
+          </CardInput>
+        ))}
 
-    return (
-        <div className="container grid grid-cols-2 gap-12 mx-auto p-4 ">
-            <SideBar />
-            <div className="grid gap-4">
-                <FormHeader onCancel={() => {}} onSave={() => {}}>
-                    <LabelledInput
-                        id="Some Name"
-                        label="Title"
-                        placeholder="Enter the Deck title here..."
-                        register={register}
-                        registrationParams="title"
-                    />
-                </FormHeader>
-                <CardInput onDelete={() => {}} orderNumber={0 + 1}>
-                    <LabelledInput
-                        label="Question"
-                        id="Question"
-                        placeholder="Place Question here"
-                        register={register}
-                        registrationParams={`card.${0}.question`}
-                    />
-                    <LabelledInput
-                        label="Answer"
-                        id="Answer"
-                        placeholder="Place the Correct Answer Here..."
-                        register={register}
-                        registrationParams={`card.${0}.answer`}
-                    />
-                </CardInput>
-            </div>
-        </div>
-    );
+        <button
+          className="px-4 py-2 bg-teal-600 rounded-md border border-white"
+          onClick={handleAddMoreCards}
+        >
+          Add More
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
--- src/components/LabelledInput.tsx
+++ src/components/LabelledInput.tsx
@@ -1,54 +1,71 @@
-import { RegisterOptions, UseFormRegister } from "react-hook-form";
+import { FieldErrors, RegisterOptions, UseFormRegister } from "react-hook-form";
 import { FormValues } from "../HomePage";
 interface LabelledInputProps {
-    label?: string;
-    placeholder?: string;
-    id: string;
-    register: UseFormRegister<FormValues>;
-    inputIdx?: number;
-    registrationParams:
-        | "title"
-        | "card"
-        | `card.${number}`
-        | `card.${number}.question`
-        | `card.${number}.answer`
-        | `card.${number}.queue`;
-    registrationOptions?: RegisterOptions<
-        FormValues,
-        | "title"
-        | "card"
-        | `card.${number}`
-        | `card.${number}.question`
-        | `card.${number}.answer`
-        | `card.${number}.queue`
-    >;
+  label?: string;
+  placeholder?: string;
+  id: string;
+  register: UseFormRegister<FormValues>;
+  inputIdx?: number;
+  registrationParams:
+    | "title"
+    | "card"
+    | `card.${number}`
+    | `card.${number}.question`
+    | `card.${number}.answer`
+    | `card.${number}.queue`;
+  registrationOptions?: RegisterOptions<
+    FormValues,
+    | "title"
+    | "card"
+    | `card.${number}`
+    | `card.${number}.question`
+    | `card.${number}.answer`
+    | `card.${number}.queue`
+  >;
+  errors?: FieldErrors<FormValues>;
 }
 
 const LabelledInput = ({
-    label = "",
-    placeholder,
-    id,
-    register,
-    registrationParams,
-    registrationOptions,
+  label = "",
+  placeholder,
+  id,
+  register,
+  registrationParams,
+  registrationOptions,
+  errors,
+  inputIdx,
 }: LabelledInputProps) => {
-    return (
-        <form className="flex gap-2 justify-center items-center">
-            {label && (
-                <label htmlFor={id} className="min-w-20 font-bold">
-                    {label}
-                </label>
-            )}
-            <input
-                placeholder={placeholder}
-                className="w-full p-2 bg-transparent outline-gray-400 ring-gray-400 border rounded-lg border-gray-400 placeholder:text-white"
-                id={id}
-                {...register(registrationParams, registrationOptions)}
-                // TODO Make this register work.
-            />
-            {/* {e}<span></span> */}
-        </form>
-    );
+  let questionError: string;
+  if (errors && errors.card) {
+    questionError = errors.card[inputIdx]?.question?.message ?  errors.card[inputIdx]?.question?.message :  errors.card[inputIdx]?.answer?.message;
+  }
+  console.log(errors);
+  
+
+  return (
+    <>
+      <form className="flex gap-2 justify-center items-center">
+        {label && (
+          <label htmlFor={id} className="min-w-20 font-bold">
+            {label}
+          </label>
+        )}
+        <input
+          placeholder={placeholder}
+          className="w-full p-2 bg-transparent outline-gray-400 ring-gray-400 border rounded-lg border-gray-400 placeholder:text-white"
+          id={id}
+          {...register(registrationParams, registrationOptions)}
+          required
+        />
+      </form>
+      {/* {errors[id] && (
+        <span className="text-red-900 align-middle">{errors[id]?.message}</span>
+      )}
+      {questionError && (
+        <span className="text-red-900 align-middle">{questionError}</span>
+      )} */}
+    </>
+  );
 };
 
 export default LabelledInput;
